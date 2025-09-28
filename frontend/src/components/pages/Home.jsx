b15559babd9e88569402bcac07ffc4ab5d23cdf6
mahidulkabir <mahidulkabir8@gmail.com>
2025-09-28 17:09:12 +0600

    added hero section

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../common/Layout';
+import Hero from '../common/Hero';
 
 const Home = () => {
     return (
         <Layout>
-            This is From Home Page 
+            <Hero/>
          </Layout>
     );
 };
