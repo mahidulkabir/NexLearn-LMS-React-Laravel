b9b9f6ab0a2bf5e2966bcd75d549112aa60231d1
mahidulkabir <mahidulkabir8@gmail.com>
2025-09-30 01:04:36 +0600

    added admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import MyCourses from './components/pages/account/MyCourses';
 import CoursesEnrolled from './components/pages/account/CoursesEnrolled'
 import WatchCourse from './components/pages/account/WatchCourse';
 import ChangePassword from './components/pages/account/ChangePassword';
+// admin panel pages 
+import AdminHome from './adminPanel/pages/AdminHome';
 
 
 function App() {
@@ -28,6 +30,10 @@ function App() {
         <Route path='/account/courses-enrolled' element={<CoursesEnrolled/>}/>
         <Route path='/account/watch-course' element={<WatchCourse/>}/>
         <Route path='/account/change-Password' element={<ChangePassword/>}/>
+
+        {/* admin pages here  */}
+
+        <Route path='/admin/dashboard' element={<AdminHome/>}/>
 
      </Routes>
      </BrowserRouter>
